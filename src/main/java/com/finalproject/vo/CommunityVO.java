5a2f56c15d3f881d75fad0e6e46b1f8da69fcebc
yujeongah <loveyuyja@naver.com>
2022-08-08 17:00:45 +0900

    second modify CommunityVO

--- src/main/java/com/finalproject/vo/CommunityVO.java
+++ src/main/java/com/finalproject/vo/CommunityVO.java
@@ -2,28 +2,32 @@ package com.finalproject.vo;
 
 import java.util.Date;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+import org.springframework.web.multipart.MultipartFile;
 public class CommunityVO  {
 	private int pid;
 	private String uid;
-	private String statid;
 	private String location;
 	private String title;
+	private String statid;
 	private String tf;
 	private String cimgname;
-	private Date regdate;
+	private Date regdate;	
+	private MultipartFile mf;
 	
+	public CommunityVO() {
+	}
+
+	public CommunityVO(int pid, String uid, String location, String title, String statid, String tf, String cimgname,
+			Date regdate) {
+		this.pid = pid;
+		this.uid = uid;
+		this.location = location;
+		this.title = title;
+		this.statid = statid;
+		this.tf = tf;
+		this.cimgname = cimgname;
+		this.regdate = regdate;
+	}
 
 	// insert
 
@@ -44,5 +48,46 @@ public class CommunityVO  {
 		this.tf = tf;
 		this.cimgname = cimgname;
 	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getStatid() {
+		return statid;
+	}
+
+	public void setStatid(String statid) {
+		this.statid = statid;
+	}
+
+	public String getCimgname() {
+		return cimgname;
+	}
+
+	public void setCimgname(String cimgname) {
+		this.cimgname = cimgname;
+	}
+
+	public MultipartFile getMf() {
+		return mf;
+	}
+
+	public void setMf(MultipartFile mf) {
+		this.mf = mf;
+	}
 	
 }
+
